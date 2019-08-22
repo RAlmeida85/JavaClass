2e22c14ddc16209ac53a5a98993b742441e2cbd0
RAlmeida85 <rafaelhbanger@gmail.com>
2019-08-22 15:18:29 +0200

    Updated version

--- 111_AutoboxingAndUnboxing/src/com/ralmeida/Main.java
+++ 111_AutoboxingAndUnboxing/src/com/ralmeida/Main.java
@@ -1,5 +1,7 @@
 package com.ralmeida;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,9 +10,29 @@ public class Main {
         // For this purpose we can use Autoboxing and Unboxing concepts
 
         // Java provide object classes for primitives types
-        Integer integer = new Integer(54);
-        Double doubleValue = new Double(12.56);
+        Integer integer = 54; //Integer.valueOf(56)
+        Double doubleValue = 12.56;
 
+        ArrayList<Integer> intArrayList = new ArrayList<Integer>();
+
+        for (int i=0; i<10; i++){
+            // we can use Integer.valueOf(int) method to AUTOBOX an int value to an Integer object
+            intArrayList.add(i);
+        }
+
+        for (int i=0; i<10; i++){
+            // we can use ArrayList.get(index).intValue() method to Unbox a Integer object value to an int variable
+            System.out.println("Integer " + i + " --> " + intArrayList.get(i));
+        }
+        ArrayList<Double> doubleList = new ArrayList<Double>();
+        for (double d=0.0; d <=5.5; d+= 0.5){
+            doubleList.add(d);
+        }
+
+        for (int i=0; i < doubleList.size(); i++){
+            double value = doubleList.get(i);
+            System.out.println("Double " + i + " --> " + value);
+        }
 
 
     }
