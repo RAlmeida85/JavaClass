2035be88641785b9fc6b85a2ff4ef39af803c6aa
RAlmeida85 <rafaelhbanger@gmail.com>
2019-08-22 14:55:44 +0200

    First commit

---
+++ 111_AutoboxingAndUnboxing/src/com/ralmeida/Main.java
@@ -0,0 +1,17 @@
+package com.ralmeida;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // ArrayList (and any other List in java expect Class objects, and we cannot use primitive types
+        // For this purpose we can use Autoboxing and Unboxing concepts
+
+        // Java provide object classes for primitives types
+        Integer integer = new Integer(54);
+        Double doubleValue = new Double(12.56);
+
+
+
+    }
+}
